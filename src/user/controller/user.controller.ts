7c8c7e4b8df5c26dd1e9ab9aff69e8bdcfe0be19
santaugusto <sanntaugusto@gmail.com>
2025-08-01 13:51:57 -0300

    feat(user): implementar update e delete de usuários via comandos e ajustar controller

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { CreateUserCommand } from '../commands/create-user.command';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { User } from '../entities/user.entity';
+import { GetUsersQuery } from '../query/get-users.query';
 
 @ApiTags('users')
 @Controller('users')
 export class UserController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @Post()
   @ApiOperation({ summary: 'Criar novo usuário' })
@@ -24,5 +28,9 @@ export class UserController {
       email: user.email,
       name: user.name ?? '',
     };
+  }
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.queryBus.execute(new GetUsersQuery('', undefined));
   }
 }
